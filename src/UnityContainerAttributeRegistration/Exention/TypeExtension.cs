57660456364da865d60a4a07af9ea8bca281f96e
Darius Weber <darius.weber@posteo.de>
2020-09-22 20:58:33 +0200

    Add TypeExtension class

---
+++ src/UnityContainerAttributeRegistration/Exention/TypeExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace UnityContainerAttributeRegistration.Exention
+{
+    internal static class TypeExtension
+    {
+        public static bool IsStatic(this Type self)
+        {
+            return self.IsAbstract && self.IsSealed;
+        }
+    }
+}
